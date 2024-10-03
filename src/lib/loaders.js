a7e49c67ca38bfa484c9992421d7d1773b89d887
Vinay Vora <122713139+wizardsWeb@users.noreply.github.com>
2024-10-03 19:43:59 +0530

    completed and fix list and singlepage

--- src/lib/loaders.js
+++ src/lib/loaders.js
@@ -1,6 +1,16 @@
+import { defer } from "react-router-dom";
 import apiRequest from "./apiRequest"
 
 export const singlePageLoader = async ({ request, params }) => {
     const res = await apiRequest.get("/posts/" + params.id);
     return res.data;
   };
+
+export const listPageLoader = async ({ request, params }) => {
+  const query = request.url.split('?')[1]
+  const postPromise = apiRequest.get("/posts?" + query);
+  return defer ({
+    postResponse: postPromise 
+  });
+  };
+
